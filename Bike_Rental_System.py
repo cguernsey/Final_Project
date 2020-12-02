0cf9c059920d3d4041ca017eac5e4f6da768fcc5
Cameron <73710960+cvcarter@users.noreply.github.com>
2020-12-02 13:40:22 -0500

    Update Bike_Rental_System.py

--- Bike_Rental_System.py
+++ Bike_Rental_System.py
@@ -277,12 +277,6 @@ def create_account():
         None
     '''
 
-    # Initialize the users list if it does not exist
-    if 'user_list' in globals() or 'user_list' in locals():
-        pass
-    else:
-        user_list = []
-
     # Create a new user object when the new account is initiated
     print("Let's create a new account. ")
     new_user = User()
@@ -299,95 +293,33 @@ def create_account():
     print("User List: ")
     print(user_list)
 
-
-def password(enc_msg, usr, passwd, N, D):
-    '''
-    This function decrypts an encrypted message if the given username
-    and password match the information in the database.
-
-    **Parameters**
-
-        *list, str, int*
-            The encrypted message list, the string username and password,
-            and the integers values of N and D
-
-    **Returns**
-
-        *str*
-            The decrypted message if username and password match
-    '''
-
-    def decrypt(enc_msg, N, D):
-        '''
-        This function will decrypt the encrypted message given with N and D
-        values.
-
-        **Parameters**
-
-            *list, int*
-                The encrypted message list as well as N and D integers
-
-        **Returns**
-
-            *str*
-                The decrypted message as a string
-        '''
-        return ''.join([chr((s ** D) % N) for s in enc_msg])
-
-    # These are the randomly generated salts for each password hash
-    salt1 = b'\xf3\x1f\xca\xbb\xa5\x11\xada\xdc\x12\xe4\x9a\xf5+\xc0\x02'
-    salt2 = b'<\xfb\x96\xcfz\xa7\xa9\xd2]\x8a?\x90\xf8!_#'
-
-    # These are the salted password hashes added to the database
-    pass1 = [b'\xf9y\r\xa99\xb0d)V\xd0\xecv_\xfc\xd1\x17(\xf0\xbe=T\xbe\xb1z'
-             b'\xb0\xc0\xfc+\xd2,\xb4\x91\x98\x917\xf8\x1f\x05\x06?+\xcc\x0b]'
-             b'\x0fB\xb5\x19\x88\xccS0x\r\x813\xc6\xe7\xab8\x8e\xc6\x1b%']
-
-    pass2 = [b'\x04\xa9\x1e\x1fPo4OZ\xe6w0!F\xcc\xa8h\x0fM\xd7\xa9\x85p\x10'
-             b'\xf9\x8a7\x08\xf4\xa4\xa5(A{\xb2\xf2~\xdf5\xa9\xb9x\x11\xebEb'
-             b'\xf7\xceo<"v\x84\xdacG*\x92\x80\xef\xf9\x82\xf5\x0e']
-
-    # Create a database of usernames, passwords, and salts
-    df = pd.DataFrame(columns=['Username', 'Password', 'Salt'])
-    new_users = pd.DataFrame([['hherbol1', pass1[0], salt1],
-                              ['cvcarter', pass2[0], salt2]],
-                             columns=['Username', 'Password', 'Salt'])
-    df = df.append(new_users, ignore_index=True)
-
-    # Check if username exists in the database and return row
-    if df['Username'].str.contains(usr).any():
-        ind = np.where(df['Username'] == usr)
-        ind = int(ind[0])
-    else:
-        raise AssertionError("Username not found")
-
-    # Create the salted hash of the password given by the user
-    passwd = str.encode(passwd)
-    hash_passwd = hashlib.pbkdf2_hmac('sha512', passwd, df.at[ind, 'Salt'],
-                                      100000)
-
-    # Loop through dataframe to check if hashed password matches
-    if hash_passwd == df.Password[ind]:
-        print("The password is accepted!")
-        return(decrypt(enc_msg, N, D))
-    else:
-        raise AssertionError("Password is incorrect")
-
         
 def rent_or_return():
-    function = input("Would you like to Rent (1) or Return (2)? \n")
+
+    # Initialize the users list if it does not exist
+    if 'user_list' in globals() or 'user_list' in locals():
+        pass
+    else:
+        user_list = []
+
+    function = input("Would you like to 'Rent' (1) or 'Return' (2)? \n")
     if function == '1' or function == 'Rent':
         
         #RENT Function Goes Here
-        
+        f2 = input(" 'Login' (1) or 'Create an Account' (2) ")
+        if f2 == '1' or f2 == 'Login':
             #LOGIN FUNCTION
-            
-            #CREATE ACCOUNT FUNCTION (__init__(new_user))
+            login()
+        elif f2 == '2' or f2 == 'Create an Account':
+            #CREATE ACCOUNT FUNCTION
+            create_account()
+                #RENT_BIKE FUNCTION
+        else:
+            print("Invalid Input. Select Login (1) or Create an Account (2) ")
+            # Send back to main login page if invalid input
+            rent_or_return()
         
-            #RENT_BIKE FUNCTION    
-            
-        
-    print("Thanks for renting with us!")
+        print("Thanks for renting with us!")
     
     elif function == '2' or function == 'Return':
         
